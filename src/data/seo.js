0088e16ba0f15120ad0d8e500829237485d3fe89
Elliott Mejia <mejia.elliott@gmail.com>
2023-09-29 15:01:51 -0700

    updated

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,35 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Elliott Mejia is an ecommerce development and marketing expert, having worked for several popular brands in the fashion space.",
+		keywords: ["Elliott", "Elliott Mejia"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Elliott Mejia is an ecommerce marketing and development specialist.",
+		keywords: ["Elliott", "Elliott Mejia", "Elliott Mejia"],
 	},
 
-	{
+	/* 	{
 		page: "articles",
 		description:
 			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+		keywords: ["Elliott", "Elliott Mejia"],
+	}, */
 
 	{
 		page: "projects",
-		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		description: "An assortment of projects by Elliott Mejia",
+		keywords: ["Elliott Mejia", "Elliott M"],
 	},
 
 	{
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["Elliott", "Elliott Mejia"],
 	},
 ];
 
